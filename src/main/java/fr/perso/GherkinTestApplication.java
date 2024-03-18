f1ff2489b39f253d93a1c17d6d2aedfba4fa1be5
francoisproust <46931405+francoisproust@users.noreply.github.com>
2024-03-18 09:36:31 +0100

    Initial commit

---
+++ src/main/java/fr/perso/GherkinTestApplication.java
@@ -0,0 +1,13 @@
+package fr.perso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GherkinTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GherkinTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/perso/GherkinTestApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.perso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GherkinTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
